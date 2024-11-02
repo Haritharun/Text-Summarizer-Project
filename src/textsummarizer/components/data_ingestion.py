3826b096b93262ce5381576fd893523d76d43497
Haritharun <1653224haritharun@gmail.com>
2024-11-03 01:04:04 +0530

    model evaluation completed

--- src/textsummarizer/components/data_ingestion.py
+++ src/textsummarizer/components/data_ingestion.py
@@ -6,12 +6,9 @@ from textSummarizer.utils.common import get_size
 from pathlib import Path
 from textSummarizer.entity import DataIngestionConfig
 
-
 class DataIngestion:
     def __init__(self, config: DataIngestionConfig):
         self.config = config
-
-
     
     def download_file(self):
         if not os.path.exists(self.config.local_data_file):
@@ -23,8 +20,6 @@ class DataIngestion:
         else:
             logger.info(f"File already exists of size: {get_size(Path(self.config.local_data_file))}")  
 
-        
-    
     def extract_zip_file(self):
         """
         zip_file_path: str
